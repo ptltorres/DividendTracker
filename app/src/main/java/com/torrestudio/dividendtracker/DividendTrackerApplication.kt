38018aa0169d671851c1af4be0c016c09cadf1b3
ptltorres <pedroltg78@hotmail.com>
2020-05-02 22:45:17 -0400

    Initial commit

---
+++ app/src/main/java/com/torrestudio/dividendtracker/DividendTrackerApplication.kt
@@ -0,0 +1,14 @@
+package com.torrestudio.dividendtracker
+
+import android.app.Application
+
+/**
+ * Created by Pedro Torres on 5/2/2020
+ */
+
+class DividendTrackerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
